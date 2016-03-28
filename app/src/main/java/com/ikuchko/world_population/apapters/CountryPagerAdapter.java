56c21377a376876de8b570ec9fc7499a87054955
Illia <ikuchko@gmail.com>
2016-03-27 17:18:43 -0700

    create CountryPagerAdapter for population Country pager

--- app/src/main/java/com/ikuchko/world_population/apapters/CountryPagerAdapter.java
+++ app/src/main/java/com/ikuchko/world_population/apapters/CountryPagerAdapter.java
@@ -1,7 +1,37 @@
 package com.ikuchko.world_population.apapters;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.ikuchko.world_population.activities.CountryDetailFragment;
+import com.ikuchko.world_population.models.Country;
+
+import java.util.ArrayList;
+
 /**
  * Created by iliak on 3/27/16.
  */
-public class CountryPagerAdapter {
+public class CountryPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Country> country;
+
+    public CountryPagerAdapter (FragmentManager fm, ArrayList<Country> country) {
+        super (fm);
+        this.country = country;
+    }
+
+    @Override
+    public Fragment getItem (int position) {
+        return CountryDetailFragment.newInstance(country.get(position));
+    }
+
+    @Override
+    public int getCount () {
+        return country.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return country.get(position).getName();
+    }
 }
